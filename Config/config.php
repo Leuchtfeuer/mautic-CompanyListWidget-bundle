2733abfec8b46a9f7838895d1c1a74c39047da84
Madlen <m.friedrich@leuchtfeuer.com>
2026-01-28 15:09:08 +0100

    bump version

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Company List Widget by Leuchtfeuer',
     'description' => 'Add Widget that lists Companies (filtered by segment and/or tag) in the Dashboard',
-    'version'     => '6.0.0',
+    'version'     => '6.0.2',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
 
     'services'    => [
