3e6fd294962c24f4fbb4364b74114b3df6e48a67
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2026-02-05 16:06:07 +0100

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Company List Widget by Leuchtfeuer',
     'description' => 'Add Widget that lists Companies (filtered by segment and/or tag) in the Dashboard',
-    'version'     => '1.1.5',
+    'version'     => '5.0.0',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
 
     'services'    => [
